f1cb91a73d2ac9c56d44f9ae6a83224e5e5be8b2
Marius <comptededev01@gmail.com>
2026-01-11 19:10:53 +0100

    Mise à jour du routage

--- public/index.php
+++ public/index.php
@@ -30,84 +30,88 @@ $connexion = new Connexion();
 $session = SessionManager::getInstance();
 $twig->addGlobal('session', $session);
 $logger = Logger::getInstance();
-if (isset($_GET['id'])) {
-    try {
-        $ArticleControlleur->article($_GET['id']);
-    } catch (InvalidArgumentException $e) {
-        $ArticleControlleur->index($e->getMessage());
-    }
-} else {
-    $uri = $_SERVER['REQUEST_URI'];
-    $logger->log($uri);
-    switch ($uri) {
-        case '/':
-        case '/accueil':
-            $ArticleControlleur->index(null);
-            break;
-        case '/connexion':
-            $userId = $session->get('user_id');
-            $logger->log("ID Session actuel : " . $userId);
-            if (!empty($userId)) {
-                $logger->log("Utilisateur déjà connecté -> Redirection Dashboard");
-                $userEmail = $session->get('email') ?? '';
-                $ConnexionControlleur->dashboard($userEmail);
-            } elseif (!empty($_POST)) {
-                $logger->log("Tentative de connexion (POST)");
-                if ($connexion->logIn()) {
-                    $logger->log("Connexion réussie pour " . $_POST['email']);
-                    $ConnexionControlleur->dashboard($_POST["email"]);
-                } else {
-                    $logger->log("Echec connexion");
-                    echo 'Mauvais email/mot de passe';
-                    $ConnexionControlleur->index();
-                }
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$logger->log($uri);
+
+switch ($uri) {
+    case '/':
+    case '/accueil':
+        $ArticleControlleur->index(null);
+        break;
+    case '/connexion':
+        $userId = $session->get('user_id');
+        $logger->log("ID Session actuel : " . $userId);
+        if (!empty($userId)) {
+            $logger->log("Utilisateur déjà connecté -> Redirection Dashboard");
+            $userEmail = $session->get('email') ?? '';
+            $ConnexionControlleur->dashboard($userEmail);
+        } elseif (!empty($_POST)) {
+            $logger->log("Tentative de connexion (POST)");
+            if ($connexion->logIn()) {
+                $logger->log("Connexion réussie pour " . $_POST['email']);
+                $ConnexionControlleur->dashboard($_POST["email"]);
             } else {
-                $logger->log("Affichage formulaire connexion");
+                $logger->log("Echec connexion");
+                echo 'Mauvais email/mot de passe';
                 $ConnexionControlleur->index();
             }
-            break;
-        case '/changerUtilisateur':
-            $ConnexionControlleur->changerUtilisateur();
-            break;
-		case '/posterCommentaire':
-			$CommentaireControlleur->posterCommentaire();
-			break;
-		case '/inscription':
-			$ConnexionControlleur->inscription();
-			break;
-		case '/traitementInscription':
-			$ConnexionControlleur->traitementInscription();
-			break;
-		case '/creer-article':
-			$ArticleControlleur->creer();
-			break;
-		case '/traitementCreation':
-			$ArticleControlleur->traitementCreation();
-			break;
-		case '/editer-article':
-			$ArticleControlleur->editer();
-			break;
-		case '/traitementEdition':
-			$ArticleControlleur->traitementEdition();
-			break;
-        case '/deconnexion':
-            $connexion->logOut();
-            $ArticleControlleur->index(null);
-            break;
-        case '/majRoles':
-            $ConnexionControlleur->majRoles();
-            break;
-        case '/supprimerUtilisateur':
-            $ConnexionControlleur->supprimerUtilisateur();
-            break;
-        case '/changerStatutArticle':
-            $ConnexionControlleur->changerStatutArticle();
-            break;
-        case '/supprimerArticle':
-            $ConnexionControlleur->supprimerArticle();
-            break;
-        default:
+        } else {
+            $logger->log("Affichage formulaire connexion");
+            $ConnexionControlleur->index();
+        }
+        break;
+    case '/changerUtilisateur':
+        $ConnexionControlleur->changerUtilisateur();
+        break;
+    case '/posterCommentaire':
+        $CommentaireControlleur->posterCommentaire();
+        break;
+    case '/inscription':
+        $ConnexionControlleur->inscription();
+        break;
+    case '/traitementInscription':
+        $ConnexionControlleur->traitementInscription();
+        break;
+    case '/creer-article':
+        $ArticleControlleur->creer();
+        break;
+    case '/traitementCreation':
+        $ArticleControlleur->traitementCreation();
+        break;
+    case '/editer-article':
+        $ArticleControlleur->editer();
+        break;
+    case '/traitementEdition':
+        $ArticleControlleur->traitementEdition();
+        break;
+    case '/deconnexion':
+        $connexion->logOut();
+        $ArticleControlleur->index(null);
+        break;
+    case '/majRoles':
+        $ConnexionControlleur->majRoles();
+        break;
+    case '/supprimerUtilisateur':
+        $ConnexionControlleur->supprimerUtilisateur();
+        break;
+    case '/changerStatutArticle':
+        $ConnexionControlleur->changerStatutArticle();
+        break;
+    case '/supprimerArticle':
+        $ConnexionControlleur->supprimerArticle();
+        break;
+    
+    default:
+        if (isset($_GET['id'])) {
+            try {
+                $ArticleControlleur->article($_GET['id']);
+            } catch (InvalidArgumentException $e) {
+                $ArticleControlleur->index($e->getMessage());
+            }
+        } else {
             http_response_code(404);
-            break;
-    }
+            echo "Page non trouvée";
+        }
+        break;
 }
